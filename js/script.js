468da1e628d14a033713267635a0159b55d6c46a
DenisBorgesGenta <144848239+DenisBorgesGenta@users.noreply.github.com>
2023-10-05 03:03:32 -0300

    Correcciones.

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
 contenido: "150ml",
 beneficios: "Mejora el proceso de reparación de la piel.", 
 tipoDePielRecomendada: "sensible",
-imagen: "./img/pleasing.png"},
+imagen: "./img/pleasing.jpeg"},
 
 {nombre: "HYALU B5 OJOS",
 contenido: "200ml",
@@ -32,7 +32,7 @@ let contenedorProd = document.createElement("div")
 contenedorProd.innerHTML = `<center><div class="card mb-3" style="max-width: 600px;">
 <div class="row g-0">
   <div class="col-md-4">
-  <img src="${productos.imagen}"></img>
+  <img src="${productos.imagen}" style="></img>
   </div>
   <div class="col-md-8">
     <div class="card-body">
@@ -46,13 +46,40 @@ contenedorProd.innerHTML = `<center><div class="card mb-3" style="max-width: 600
 </div></center>`
 
 contenedor.appendChild(contenedorProd)
-});}
+});
 
+// Estoy intentando guardar datos y mostrarlos guiándome con la clase de storage dada por Diana y no lo estoy logrando.
+// El push al array figura vacío.
+
+class datosNewsletter {
+constructor(nombre, apellido, email)
+{this.nombre = nombre; 
+this.apellido = apellido;
+this.email = email;}}
+
+
+let datosIngresados = [];
+
+let formularioDatosNewsletter = document.getElementById("formularioNewsletter");
+formularioDatosNewsletter.addEventListener("submit", guardarDatos);
+
+function guardarDatos(e){
+e.preventDefault();
+let nombre = document.getElementById("ingresarNombre").value;
+let apellido = document.getElementById("ingresarApellido").value;
+let email = document.getElementById("ingresarEmail").value;
+
+function pushearDatos ()
+{let nuevosDatos  = (new datosNewsletter(nombre, apellido, email));
+nuevosDatos.push(datosI);}
+
+console.log(datosIngresados)
+
+
+let container = document.getElementById("divNews");
+container.textContent="";
+datosIngresados.forEach((dato)=>{
+let card = document.createElement("div");
+})}}
 
 simulaciontienda();
-
-
-
-
-    
-
